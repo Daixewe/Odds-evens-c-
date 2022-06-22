cd9a2c784fb0ec69542685889f7e089f2b65d31c
Daixewe <fuentesvd56@gmail.com>
2022-06-22 14:18:07 -0600

    odds or evens in c#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string input = Console.ReadLine();
+
+int number = Int32.Parse(input);
+
+int module = number % 2;
+
+
+if (module == 0)
+{
+    Console.WriteLine("Even");
+}  else  {
+    Console.WriteLine("odd");
+}
+
